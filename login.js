86a7170ef5a09c21eee5fe15f20995f229eca093
JooJ <50976403+JoooooooooJ@users.noreply.github.com>
2021-04-13 21:12:26 -0300

    realizando login com ajax

--- login.js
+++ login.js
@@ -1,14 +1,28 @@
-$(document).ready(function() {
-    var user = $("#user").val()
-    var pass = $("#password").val()
+$(document).ready(() => {
+    $("#sign-in-button").on('click', () => {
+        var user = $("#user").val()
+        var pass = $("#password").val()
 
-    $("#login").on('submit', function() {
-        $.post("https://reqres.in/api/login", {
+        $.ajax({
+            type: "POST",
+            url: 'https://reqres.in/api/login',
+            dataType: "json",
+            data: JSON.stringify({
                 "email": user,
                 "password": pass
-            },
-            function(data, status) {
+            }),
+            contentType: "application/json; charset=utf-8",
+            success: (data) => {
                 localStorage.setItem("token", data.token)
-            })
+                window.open("../joooooooooj.github.io/index.html", "_self")
+            },
+            error: (data) => {
+                const errorbox = $("#error-message")
+                const error = data.responseJSON.error
+                errorbox.removeClass("d-none")
+                errorbox.addClass("d-block")
+                errorbox.html(error)
+            }
+        });
     })
 })
