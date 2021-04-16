eb17f78d12d6fcac6cccd75a2d319841c5015b49
JooJ <50976403+JoooooooooJ@users.noreply.github.com>
2021-04-16 18:31:55 -0300

    adicionando pesquisa na api do github

--- search.js
+++ search.js
@@ -1,45 +1,28 @@
-$(document).ready(() => {
-    var timer = null
-    var queryElement = $("#searchQuery")
-    queryElement.on("keyup", () => {
-        var search = queryElement.val()
-        var request = () => {
-            $.ajax({
-                type: "GET",
-                url: `https://api.github.com/search/repositories?q=${search}`,
-                dataType: "json",
-                contentType: "application/json; charset=utf-8",
-                success: (data) => {
-                    data.items.foreach(item => {
-                        var ul = document.getElementById("search");
-                        var li = document.createElement("li");
-                        li.innerHTML = `<a href="${item.html_url}">${item.full_name}</a>`
-                        ul.appendChild(li);
-                    })
-                },
-                error: (data) => {
-                    console.log(data.responseJSON)
-                }
-            });
-        }
-        clearTimeout(timer);
-        timer = setTimeout(request, 500);
-    })
-})
-
+var timer = null
 
 function pesquisar() {
-    var input, filtro, menu, menuItens, links;
-    input = document.getElementById("search");
-    filtro = input.value.toUpperCase();
-    menu = document.getElementById("menu");
-    menuItens = menu.getElementsByTagName("li");
-    for (var i = 0; i < menuItens.length; i++) {
-        links = menuItens[i].getElementsByTagName("a")[0];
-        if (links.innerHTML.toUpperCase().indexOf(filtro) > -1) {
-            menuItens[i].style.display = "";
-        } else {
-            menuItens[i].style.display = "none";
-        }
+    var search = $("#search").val()
+    var request = () => {
+        $.ajax({
+            type: "GET",
+            url: `https://api.github.com/search/repositories?q=${search}`,
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: (data) => {
+                console.log(data)
+                data.items.forEach(item => {
+                    console.log(item)
+                    var ul = document.getElementById("menu");
+                    var li = document.createElement("li");
+                    li.innerHTML = `<a href="${item.html_url}">${item.full_name}</a>`
+                    ul.appendChild(li);
+                })
+            },
+            error: (data) => {
+                console.log(data.responseJSON)
+            }
+        });
     }
+    clearTimeout(timer);
+    timer = setTimeout(request, 500);
 }
