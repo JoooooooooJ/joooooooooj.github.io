031a3a78d6c38fec96d5e4ec71748979c54003cc
CesarO99 <cesar.augustooliveira2010@hotmail.com>
2021-04-25 17:53:48 -0300

    searchs final

--- search.js
+++ search.js
@@ -2,7 +2,7 @@ var timer = null
 
 function pesquisar() {
     var search = $("#search").val()
-    if(search.toString().lenght > 3){
+    if(search.toString().length > 3){
     var request = () => {
         $.ajax({
             type: "GET",
@@ -10,11 +10,9 @@ function pesquisar() {
             dataType: "json",
             contentType: "application/json; charset=utf-8",
             success: (data) => {
-                console.log(data)
                 var ul = document.getElementById("menu");
                 ul.innerHTML = ''
                 data.items.forEach(item => {
-                    console.log(item)
                     var li = document.createElement("li");
                     li.innerHTML = `<a href="${item.html_url}">${item.full_name}</a>`
                     ul.appendChild(li);
