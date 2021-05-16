395dd8ffb8fcb67f702639e77e549f5eb58e476a
JooJ <50976403+JoooooooooJ@users.noreply.github.com>
2021-05-16 15:51:40 -0300

    correções package.json

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const customExpress = require('../config/customExpress')
-const conexao = require('../banco/conexao')
-const Tabelas = require('../banco/tabelas')
-const Repositorio = require('../models/modelRepositorios')
+const customExpress = require('./config/custom-express')
+const conexao = require('./banco/conexao')
+const Tabelas = require('./banco/tabelas')
+const Repositorio = require('./models/model-repositorios')
 
 conexao.connect(erro => {
-    if(erro) {
+    if (erro) {
         console.log(erro)
     } else {
         console.log('conectado com sucesso')
-        
+
         Tabelas.init(conexao)
-        
+
         const app = customExpress()
 
+        app.use("/", customExpress.static(__dirname))
         app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
     }
 })
--- search.js
+++ search.js
@@ -2,28 +2,28 @@ var timer = null
 
 function pesquisar() {
     var search = $("#search").val()
-    if(search.toString().length > 3){
-    var request = () => {
-        $.ajax({
-            type: "GET",
-            url: `https://api.github.com/search/repositories?q=${search}`,
-            dataType: "json",
-            contentType: "application/json; charset=utf-8",
-            success: (data) => {
-                var ul = document.getElementById("menu");
-                ul.innerHTML = ''
-                data.items.forEach(item => {
-                    var li = document.createElement("li");
-                    li.innerHTML = `<a href="${item.html_url}">${item.full_name}</a>`
-                    ul.appendChild(li);
-                })
-            },
-            error: (data) => {
-                console.log(data.responseJSON)
-            }
-        });
+    if (search.toString().length > 3) {
+        var request = () => {
+            $.ajax({
+                type: "GET",
+                url: `https://api.github.com/search/repositories?q=${search}`,
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                success: (data) => {
+                    var ul = document.getElementById("menu");
+                    ul.innerHTML = ''
+                    data.items.forEach(item => {
+                        var li = document.createElement("li");
+                        li.innerHTML = `<a href="${item.html_url}">${item.full_name}</a>`
+                        ul.appendChild(li);
+                    })
+                },
+                error: (data) => {
+                    console.log(data.responseJSON)
+                }
+            });
+        }
+        clearTimeout(timer);
+        timer = setTimeout(request, 500);
     }
-    clearTimeout(timer);
-    timer = setTimeout(request, 500);
-}
 }
